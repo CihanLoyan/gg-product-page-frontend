f13ee17a8d4def07460e4cc7da6ef86db996b205
Cihan Loyan <cihanloyan@gmail.com>
2021-09-23 17:00:58 +0300

    css added

--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -4,11 +4,11 @@ export default function Gallery({images}) {
     return (
         <div className="gallery">
             <div className="big-image">
-                <img src={`./${images[0]}`} />
+                <img src={`./${images[0]}`} className="big-img" />
             </div>
             <div className="thumbnail-wrapper">
                 {images.map((image) => {
-                    return <img src={`./${image}`}/>       
+                    return <ul><li><img src={`./${image}`} className="small-img" /></li></ul>       
                 })}
             </div>
         </div>
--- src/components/Title/title.js
+++ src/components/Title/title.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title(props) {
     return (
         <div className="title">
-            <h2>{props.title}</h2>
+            <h2 className="header">{props.title}</h2>
         </div>
     )
 }
